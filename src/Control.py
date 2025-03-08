ee665c37df52abd6b3ee898038bb76aabeb41974
Daniel Klein <kleindaniel@live.com>
2025-03-08 17:24:14 -0300

    create order releaser

--- src/Control.py
+++ src/Control.py
@@ -1,17 +1,40 @@
 import simpy
 
+class ProductionOrder:
+    _wip_counter = 0
+
+    def __init__(self):
+        self.id = ProductionOrder._wip_counter
+        ProductionOrder._wip_counter += 1
+
+        self.product: str
+        self.schedule: int
+        self.released: int
+        self.duedate: float
+        self.finished: bool
+        self.quantity: int
+        self.priority: int
+        self.process_total: int
+        self.process_finished: int
+        self.processes: dict
+
+    def to_dict(self) -> dict:
+        return self.__dict__
 
 class Info:
     def __init__(
         self,
-        env,
+        env: simpy.Environment,
         resources: dict,
         products: dict,
     ):
         self.env = env
         self.resources = resources
         self.products = products
+
         self._create_process_data()
+        self._create_resources_stores()
+        self._create_products_stores()
 
     def _create_process_data(self) -> None:
         self.processes_name_list = {}
@@ -24,12 +47,12 @@ class Info:
             self.processes_value_list[product] = list(processes.values())
 
     def _create_resources_stores(self) -> None:
-        # Queues
+        
         self.resource_output = {}
         self.resource_input = {}
-        # KPIs
         self.resource_utilization = {}
         self.resource_breakdowns = {}
+
         for resource in self.resources:
             self.resource_output[resource] = simpy.FilterStore(self.env)
             self.resource_input[resource] = simpy.FilterStore(self.env)
@@ -57,26 +80,74 @@ class Info:
             self.lost_sales[product] = simpy.Container(self.env)
             self.wip[product] = simpy.Container(self.env)
 
+class Controller:
+    def __init__(self, env: simpy.Environment, info: Info):
+        self.env = env
+        self.info = info
+    
+    def release_production_order(
+        self,
+        product,
+        quantity,
+        schedule = 0,
+        duedate = 0,
+        priority = 0,
+    ):  
+        
+        order = self._make_production_order(
+            product = product,
+            quantity = quantity,
+            schedule = schedule,
+            duedate = duedate,
+            priority = priority
+        )
+        
+        self.env.process(self._process_production_order(order))
 
-class ProductionOrder:
-    _wip_counter = 0
+        
+    def _make_production_order(
+        self,
+        product,
+        quantity,
+        schedule = 0,
+        released = -1,
+        duedate = 0,
+        finished = False,
+        priority = 0,
+    ):
 
-    def __init__(self, product, quantity, products_config):
-        self.id = ProductionOrder._wip_counter
-        ProductionOrder._wip_counter += 1
+        production_order = ProductionOrder()
+        production_order.product = product
+        production_order.schedule = schedule
+        production_order.released = released
+        production_order.duedate = duedate
+        production_order.finished = finished
+        production_order.quantity = quantity
+        production_order.priority = priority 
+        production_order.process_total = len(
+            self.info.products[product]["processes"]
+        )
+        production_order.process_finished = 0
 
-        self.product = product
-        self.schedule = 0
-        self.released = -1
-        self.duedate = 0
-        self.finished = False
-        self.quantity = quantity
-        self.priority = 0
-        self.process_total = len(products_config[product]["processes"])
-        self.process_finished = 0
-        self.processes = {}
-        for process in products_config[product]["processes"]:
-            self.processes[process] = -1
+        return production_order
 
-    def to_dict(self) -> dict:
-        return self.__dict__
+
+    def _process_production_order(self, order: ProductionOrder):
+        product = order.product
+        schedule = order.schedule
+
+        first_process = next(iter(self.info.products[product]["processes"])) 
+        first_resource = self.info.products[product]["processes"][first_process]["resource"]
+        if schedule > self.env.now:
+            delay = schedule - self.env.now
+            yield self.env.timeout(delay)
+        else:
+            order.released = self.env.now
+                
+        # Add order to first resource input
+        yield self.info.resource_input[first_resource].put(order)
+    
+    
+
+
+
