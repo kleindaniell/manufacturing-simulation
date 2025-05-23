63097549c2cac759e2caf134a6537c0dd1a04c62
Daniel Klein <kleindaniel@live.com>
2025-05-23 09:24:04 -0300

    feat: starg dbr scheduler

--- src/rlsim/scheduler_dbr.py
+++ src/rlsim/scheduler_dbr.py
@@ -6,4 +6,8 @@ from rlsim.scheduler import Scheduler
 
 
 class DBR(Scheduler):
-    
+    def __init__(self, store, interval):
+        super().__init__(store, interval)
+
+    def _scheduler(self, interval):
+        return super()._scheduler(interval)
